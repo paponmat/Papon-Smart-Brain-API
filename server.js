7bd1bc6a06a1e8b1fbd161892e81982a9aad23b0
Papon Mateevorakulkij <furby153@gmail.com>
2023-08-23 17:45:13 +0700

    edit signin route

--- server.js
+++ server.js
@@ -3,6 +3,10 @@ const cors = require('cors')
 const bcrypt = require('bcrypt');
 const knex = require('knex');
 
+const app = express();
+app.use(express.json());
+app.use(cors());
+
 const db = knex({
     client: 'pg',
     connection: {
@@ -14,65 +18,37 @@ const db = knex({
     }
 });
 
-db.select('*').from('users').then(data => {
-    console.log(data);
-});
-
-const app = express();
-app.use(express.json());
-app.use(cors());
-
-const database = {
-    users: [
-        {
-            id: '123',
-            name: 'John',
-            email: 'john@gmail.com',
-            password: 'cookies',
-            entries: 0,
-            joined: new Date(),
-        } , 
-        {
-            id: '124',
-            name: 'Sally',
-            email: 'sally@gmail.com',
-            password: 'bananas',
-            entries: 0,
-            joined: new Date(),
-        } 
-    ],
-    login: [
-        {
-            id: '987',
-            hash: '',
-            email: 'john@gmail.com',
-        }
-    ]
-}
-
 app.get('/', (req , res) => {
-    res.send(database.users);
+    res.send('API is working');
 })
 
-app.post('/signin', (req, res) => {
-    // // Load hash from your password DB.
-    // bcrypt.compare(myPlaintextPassword, hash, function(err, result) {
-    //     // result == true
-    // });
-    // bcrypt.compare(someOtherPlaintextPassword, hash, function(err, result) {
-    //     // result == false
-    // });
-
-
-    if (req.body.email === database.users[0].email &&
-        req.body.password === database.users[0].password) {
-            // res.json('success');
-            res.json(database.users[0]);
-    } else {
-        res.status(400).json('error logging in');
+app.post('/signin', async (req, res) => {
+    const { email, password } = req.body;
+    try {
+        const data = await db.select('email', 'hash')
+            .from('login')
+            .where('email', email);
+        
+        if (data.length > 0) {
+            const isValid = await bcrypt.compare(password, data[0].hash);
+            
+            if (isValid) {
+                const user = await db.select('*').from('users').where('email', email);
+                if (user.length > 0) {
+                    res.json(user[0]);
+                } else {
+                    res.status(400).json('Unable to get user');
+                }
+            } else {
+                res.status(400).json('Invalid credentials');
+            }
+        } else {
+            res.status(400).json('Invalid credentials');
+        }
+    } catch (err) {
+        res.status(500).json('Error processing request');
     }
-    
-})
+});
 
 app.post('/register', async (req, res) => {
     const { email, name, password } = req.body;
@@ -81,41 +57,33 @@ app.post('/register', async (req, res) => {
         const saltRounds = 10; // Define the number of salt rounds for bcrypt
         
         // Hash the password using bcrypt
-        const hashFromBcrypt = await new Promise((resolve, reject) => {
-            bcrypt.hash(password, saltRounds, function(err, hash) {
-                if (err) reject(err);
-                resolve(hash);
-            });
-        });
+        const hashedPassword = await bcrypt.hash(password, saltRounds);
 
         // Start a transaction
         await db.transaction(async trx => {
             // Insert hashed password and email into 'login' table
             const login = await db('login')
-                .returning('*')
+                .returning('email')
                 .insert({
-                    hash: hashFromBcrypt,
+                    hash: hashedPassword,
                     email: email
                 })
                 .transacting(trx);
-            
-            // Retrieve the registered email
-            const loginEmail = login[0].email;
             
             // Insert user information into 'users' table
             const user = await db('users')
                 .returning('*')
                 .insert({
-                    email: loginEmail,
+                    email: login[0].email,
                     name: name,
                     joined: new Date()
                 })
                 .transacting(trx);
 
-            if (user.length > 0 && login.length) {
+            if (user.length) {
                 res.json(user[0]);
             } else {
-                res.status(404).json('Your information cannot be registered');
+                res.status(500).json('Registration failed');
             }
         });
     } catch (error) {
@@ -126,9 +94,9 @@ app.post('/register', async (req, res) => {
 app.get('/profile/:id', async (req, res) => {
     const { id } = req.params;
     try {
-        const user = await db.select('*').from('users').where('id', id);
+        const user = await db.select('*').from('users').where('id', id).first();
         
-        if (user.length > 0) {
+        if (user) {
             res.json(user[0]);
         } else {
             res.status(404).json('User not found');
@@ -142,26 +110,22 @@ app.get('/profile/:id', async (req, res) => {
 app.put('/image', async (req, res) => {
     const { id } = req.body;
     try {
-        const entries = await db('users').where('id', id)
-        .increment('entries', 1)
-        .returning('entries');
-        if (entries.length > 0) {
-            res.json(entries[0].entries);
+        const updated  = await db('users')
+            .where('id', id)
+            .increment('entries', 1)
+            .returning('entries');
+
+        if (updated.length) {
+            res.json(updated [0].entries);
+        } else {
+            res.status(400).json('Unable to update entry count');
         }
     } catch (error) {
-        res.status(400).json('Unable to get entries');
+        res.status(500).json('Internal server error');
     }
-})
-
-app.listen(3000, ()=> {
-    console.log('app is running on port 3000');
 });
 
-/*
-/ --> res = this is working
-/signin --> POST = success/fail
-/register --> POST = user
-/profile/:userID --> GET = user
-/image --> PUT --> user
-
-*/
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, ()=> {
+    console.log(`App is running on port ${PORT}`);
+});
