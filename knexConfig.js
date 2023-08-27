dc2734f16419b6a411411a0ae574be6415df47e5
Papon Mateevorakulkij <furby153@gmail.com>
2023-08-27 12:29:19 +0700

    add knexConfig.js

---
+++ knexConfig.js
@@ -0,0 +1,10 @@
+module.exports = {
+    client: 'pg', // PostgreSQL
+    connection: {
+      host : '127.0.0.1', //localhost
+      port : 5432,
+      user : 'postgres',
+      password : 'test',
+      database : 'smart-brain'
+    },
+  };
