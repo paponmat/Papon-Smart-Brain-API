5b2ef55e05c9c01a93caa78ccb64bfece19a840f
Papon Mateevorakulkij <furby153@gmail.com>
2023-08-31 12:12:46 +0700

    edit environment

--- config/knexConfig.js
+++ config/knexConfig.js
@@ -1,14 +1,18 @@
 const fs = require('fs');
+const KNEXCONFIG_HOST = process.env.KNEXCONFIG_HOST;
+const KNEXCONFIG_USER = process.env.KNEXCONFIG_USER;
+const KNEXCONFIG_DATABASE = process.env.KNEXCONFIG_DATABASE;
+const KNEXCONFIG_PASSWORD = process.env.KNEXCONFIG_PASSWORD;
 
 module.exports = {
   client: 'pg',
   connection: {
-    host: 'dpg-cjlda0fv9s6c73cba5n0-a.singapore-postgres.render.com',
+    host: KNEXCONFIG_HOST,
     port: 5432,
-    user: 'paponmat',
-    database: 'smartbrain_60pw',
-    password: 'apauJ2QQ2kPCITIWAwLCx8gjUzdk2MhP',
-    ssl: { ca: fs.readFileSync('./config/server.crt'),} 
+    user: KNEXCONFIG_USER,
+    database: KNEXCONFIG_DATABASE,
+    password: KNEXCONFIG_PASSWORD,
+    ssl: { ca: fs.readFileSync('/etc/secrets/server.crt'),} 
     ? { rejectUnauthorized: false } : false,
     },
 };
